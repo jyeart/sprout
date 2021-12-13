998a91b389e4f6889f200044a5dc1902d0050400
jye <jaymes.m.harrison@gmail.com>
2021-12-13 23:13:08 +0000

    okay, here it goes. trying to add the hello world code

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//here is the first code I am writing for my new job as a software developer!!! OMG. 
+cout << "Hello World!" << endl;
+return 0;
+}
+
