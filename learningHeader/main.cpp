27d20e14b61029c1b33deb06c5eb500a5cb5308d
jye <jaymes.m.harrison@gmail.com>
2021-12-27 04:59:32 +0000

    Okay, now the code asks all the questions and inserts the data into the database. I still have the automatically intialized values. Each time the code is built and run, it adds more to the existing database (this is creating duplicated values at this point) and it only prints to the screen newly entered values.

--- learningHeader/main.cpp
+++ learningHeader/main.cpp
@@ -37,6 +37,10 @@ int main()
     }
 
 
+//here is where all the values that will be placed in the database are being initialized, and the user is asked for input. Eventually this will all be a function inside of an object... a method I believe it is called. For now, all the program will be proceedural.
+
+    //intitializing the holder variable for the data that follows. This will be in a format that SQlite will understand.
+    string stringSQL;
 
     string char_name;
     string char_level = "3";
@@ -45,37 +49,40 @@ int main()
     string char_class = "triologist";
     cout << "Let's Make a Character! \n Enter your Character's Name:" << endl;
     cin >> char_name;
-    cout << "you entered " << char_name << endl;
+    cout << "What is their level?" << endl;
+    cin >> char_level;
+        cout << "How old are they?" << endl;
+    cin >> char_age;
+    cout << "From what realm do they haileth?" << endl;
+    cin >> char_realm;
+    cout << "And what is their occupation?" << endl;
+    cin >> char_class;
 
-    cout << "what follows is the table itself, which does not yet contain the values entered by the user" + char_name << endl;
+    // now filling the variable so that its format is one that will make sense to the database.
+
+stringSQL = "insert into myTable (CharName, Level, Age, Realm, Class) values ('" + char_name+ "', " + char_level + ", " + char_age + ", '" + char_realm + "', '" + char_class + "')";
+
+// just a print test to make sure formatting is good. Will need
+    cout << stringSQL << endl;
 
 
     //the table is created here, but I am still learning how to transfer the C variables into the database.
 
-    pSQL[0] = "create table myTable (CharName varchar(30), Level smallint, Age smallint, Realm varchar(30), Class varchar(30))";
+    pSQL[0] = "CREATE TABLE IF NOT EXISTS myTable (CharName varchar(30), Level smallint, Age smallint, Realm varchar(30), Class varchar(30))";
 
 	pSQL[1] = "insert into myTable (CharName, Level, Age, Realm, Class) values ('Tom', 1, 22, 'Morland', 'Rogue, Haha')";
 
 	pSQL[2] = "insert into myTable (CharName, Level, Age, Realm, Class) values ('Lucky', 1, 41, 'Weird', 'Chaos Priest')";
 
-//creating the user entry
-    //creating a new string to hold the value.
-    //string strSQL;
-    //strSQL =
-    //strSQL =  + char_name + "', " + char_level + ", " + char_age + ", '" + char_realm + "', '" + char_class +"')";
-    string stringSQL;
-    //stringSQL = "insert into myTable (CharName, Level, Age, Realm, Class) values ('" + char_name+ "', " + char_level + ", " + char_age + ", '" + char_realm + "', '" + char_class +"')";
 
-    stringSQL = "insert into myTable (CharName, Level, Age, Realm, Class) values ('" + char_name+ "', " + char_level + ", " + char_age + ", '" + char_realm + "', '" + char_class + "')";
-
-    cout << stringSQL << endl;
+// here is the user entered information.
     pSQL[3] = stringSQL.c_str();
 
 	pSQL[4] = "select * from myTable";
 
-	pSQL[5] = "delete from myTable";
+	//pSQL[5] = "delete from myTable";
 
-	pSQL[6] = "drop table myTable";
+	//pSQL[6] = "drop table myTable";
 
 	for(int i = 0; i < STATEMENTS; i++)
 	{
